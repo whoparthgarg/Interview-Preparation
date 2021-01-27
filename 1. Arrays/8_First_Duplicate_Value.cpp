33e43a937ba1b682a3562b3523fc0d8da799d657
Parth Garg <71939449+whoparthgarg@users.noreply.github.com>
2021-01-27 23:46:52 +0530

    Update 8_First_Duplicate_Value.cpp

--- 1. Arrays/8_First_Duplicate_Value.cpp
+++ 1. Arrays/8_First_Duplicate_Value.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
+
 int firstDuplicate(int a[],int n)
 {
     int i,j;
+    
     for(i=0;i<n-1;i++)
     {
         for(j=i+1;j<n;j++)
@@ -16,11 +18,13 @@ int firstDuplicate(int a[],int n)
 
     return -1;
 }
+
 int main()
 {
     int arr[]={2,1,3,4,3,2,5,2};
     int n=sizeof(arr)/sizeof(int);
 
     cout<<firstDuplicate(arr,n);
+    
     return 0;    
 }
