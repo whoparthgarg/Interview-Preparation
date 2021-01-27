8c318aa1cc37d5b4a94b9de606cd1e3333413c3d
whoparthgarg <parthgarg2013@gmail.com>
2021-01-27 23:44:00 +0530

    Duplicate value

---
+++ 1. Arrays/8_First_Duplicate_Value.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int firstDuplicate(int a[],int n)
+{
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                return a[i];
+            }
+        }
+    }
+
+    return -1;
+}
+int main()
+{
+    int arr[]={2,1,3,4,3,2,5,2};
+    int n=sizeof(arr)/sizeof(int);
+
+    cout<<firstDuplicate(arr,n);
+    return 0;    
+}
