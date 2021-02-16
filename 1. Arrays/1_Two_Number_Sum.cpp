7f28d299d20ffe18ed5f848b359f5859c3183c10
whoparthgarg <parthgarg2013@gmail.com>
2021-02-16 10:38:48 +0530

    Arrays

---
+++ 1. Arrays/1_Two_Number_Sum.cpp
@@ -0,0 +1,55 @@
+/*
+1st method : use two loops and get the sum (n^2)
+2nd method : use one for loop and binary search (nlogn)
+3rd method : two pointer approach only for sorted arrays (n)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void twoNumberSum(int *a,int n,int k)
+{
+    int i=0;
+    int j=n-1;
+
+    while(i<j)
+    {
+        int sum=a[i]+a[j];
+        if(sum==k)
+        {
+            cout<<a[i]<<" "<<a[j]<<endl;
+            i++;
+            j--;
+        }
+        else if(sum>k)
+        {
+            j--;
+        }
+        else if(sum<k)
+        {
+            i++;
+        }
+    }
+
+    return ;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[1000]{0};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    int k;
+    cin>>k;
+
+    twoNumberSum(arr,n,k);
+    return 0;
+}
